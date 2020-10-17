053fa4d0aad8526281f89ec7656f53160214b20b
kmb5 <20370225+kmb5@users.noreply.github.com>
2020-10-17 11:41:07 +0200

    added main game loop, finished objects

--- classes.py
+++ classes.py
@@ -4,11 +4,19 @@ class Board():
     def __init__(self, size):
         self.size = size
         self.board = self.generate_board()
+        self.move_count = 0
+        self.game_over = False
 
     def __repr__(self):
         repr_str = '\n'
         for row in self.board:
-            repr_str += ' '.join([str(i) for i in row])
+            if self.size % 3 == 0:
+                str_between = '   '
+            elif self.size % 2 == 0:
+                str_between = '  '
+            else:
+                str_between = '   '
+            repr_str += str_between.join([str(i) for i in row])
             repr_str += '\n\n'
         repr_str += '\n'
         return repr_str
@@ -28,20 +36,56 @@ class Board():
             if isinstance(self.board[i][j], Player):
                 return 'invalid move'
             self.board[i][j] = player
+            self.move_count += 1
+            
+            won = self.check_win(i, j, player)
+            if isinstance(won, Player):
+                self.game_over = True
+                print(self)
+                return f'{str(player)} won!'
+            if won == 'draw':
+                self.game_over = True
+                print(self)
+                return "It's a draw!"
             return self
         except IndexError:
             return 'invalid move'
 
     def check_win(self, last_move_x, last_move_y, player):
 
-        for i in range(len(self.size)):
+        # check rows
+        for i in range(self.size):
             if self.board[last_move_x][i] != player:
                 break
             if i == self.size - 1:
                 return player
 
-        
+        # check col
+        for i in range(self.size):
+            if self.board[i][last_move_y] != player:
+                break
+            if i == self.size - 1:
+                return player
 
+        # check diagonal
+        if last_move_x == last_move_y:
+            for i in range(self.size):
+                if self.board[i][i] != player:
+                    break
+                if i == self.size - 1:
+                    return player
+
+        # check anti diagonal
+        if last_move_x + last_move_y == self.size - 1:
+            for i in range(self.size):
+                if self.board[i][(self.size-1) - i] != player:
+                    break
+                if i == self.size - 1:
+                    return player
+
+        # check draw
+        if self.move_count == pow(self.size, 2):
+            return 'draw'
 
 class Player():
     def __init__(self, character):
@@ -51,5 +95,8 @@ class Player():
         return self.character
 
     def __str__(self):
+        return self.character
+
+    def to_str(self):
         return self.character
 
--- main.py
+++ main.py
@@ -2,14 +2,85 @@ from classes import Board, Player
 
 def main():
 
-    board = Board(3)
-    player_a = Player('X')
-    player_b = Player('O')
+    board_size = select_board_size()
+    board = Board(board_size)
 
-    print(board)
+    player_characters = select_characters()
 
-    print(board.place_move(0, 0, player_a))
-    print(board.place_move(1, 0, player_b))
+    player_a = Player(player_characters[0])
+    player_b = Player(player_characters[1])
+
+    previous_player = player_b
+
+    while not board.game_over:
+        next_player = player_a if previous_player == player_b else player_b
+        round_played = ''
+
+        while round_played in ('', 'invalid move'):
+            round_played = play_round(next_player, board)
+            print(round_played)
+            
+        previous_player = next_player
+
+
+def play_round(player, board):
+
+    print(f'\nPlayer {player} turn.\n')
+
+    move = []
+
+    for i in range(2):
+        move_i = ''
+        row_or_col = 'row' if i == 0 else 'column'
+        while move_i not in [str(x) for x in range(board.size)]:
+            move_i = input(f'Select {row_or_col} (1-{board.size}): ')
+            try:
+                move_i = int(move_i) - 1
+                move.append(move_i)
+                break
+            except (ValueError, IndexError):
+                continue
+
+    return board.place_move(move[0], move[1], player)
+
+def select_board_size(): 
+    board_size = 0
+    while board_size not in range(2,21):
+        try:
+            board_size = int(input('Select a board size between 2 and 20: '))
+        except ValueError:
+            continue
+    return board_size
+
+def select_characters():
+
+    possible_characters = [
+        'X', 'O', '♥️', '✔️', '❌', '💔', '🅾️', '👻', '🎃', '🔥'
+    ]
+
+    player_characters = []
+
+    for i in range(1, 3):
+        select_string = '\n'.join([f'{str(x + 1)}: {possible_characters[x]}'  for x in range(len(possible_characters))])
+        error_msg = f'You must select a number between 1-{len(possible_characters)}: '
+        player_select = ''
+
+        while player_select not in [str(x) for x in range(1, len(possible_characters) + 1)]:
+            print(select_string)
+            player_select = input(f'Player {i} - select your character (type from the numbers above): ')
+            try:
+                player_select = int(player_select) - 1
+                player_character = possible_characters.pop(player_select)
+                player_characters.append(player_character)
+                print(f'Player {i} - you will play as {player_character}\n')
+                break
+            except (ValueError, IndexError):
+                print(error_msg)
+                continue
+
+    return player_characters
+
+    
 
 if __name__ == "__main__":
     main()
