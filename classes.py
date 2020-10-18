7a7bfa9be3d33892e7878c31e7b9954f5c9ea0a6
kmb5 <20370225+kmb5@users.noreply.github.com>
2020-10-18 13:01:18 +0200

    Improved display of board, added docstrings & comments

--- classes.py
+++ classes.py
@@ -1,13 +1,157 @@
-
+"""This module has the all the classes needed for the game.
+"""
 
 class Board():
+    """Main class for the Tic-Tac Toe Board.
+    It can generate an arbitrary-sized board,
+    handle player moves (place Player instance in board),
+    handle game over (win/draw), and generate a nice
+    string representation of the board
+    """
     def __init__(self, size):
         self.size = size
-        self.board = self.generate_board()
         self.move_count = 0
         self.game_over = False
+        self.board = self.generate_board()
 
     def __repr__(self):
+        """If we call print() on the board
+        it will output a nice string (which is
+        used during gameplay).
+        """
+        repr_str = ''
+        for i, row in enumerate(self.board):
+            # for each even row, we add 3 lines:
+            # |- - -|
+            # |     |  (repeated n times, n = num_rows)
+            # |- - -|  (so then we end up with rows=cols)
+            if i % 2 == 0:
+                repr_str += '|'
+                repr_str += ('').join(['- - -|' for x in row])
+                # ('|- - -|- - -|- - -|\n')
+                repr_str += '\n'
+                repr_str += '|'
+                repr_str += ('').join([f'  {x}  |' for x in row])
+                repr_str += '\n'
+                repr_str += '|'
+                repr_str += ('').join(['- - -|' for x in row])
+                repr_str += '\n'
+            # for each odd row, we only add 1 line:
+            # |     |  (repeated n times, n = num_rows)
+            else:
+                repr_str += '|'
+                repr_str += ('').join([f'  {x}  |' for x in row])
+                repr_str += '\n'
+
+        if len(self.board) % 2 == 0:
+            # if the board has an even number of rows,
+            # we need to add a last, closing line:
+            # |- - -| * n (repeated n times, n = num_rows)
+            repr_str += '|'
+            repr_str += ('').join(['- - -|' for x in row])
+            repr_str += '\n'
+
+        return repr_str
+
+    def generate_board(self):
+        """Creates a 2-d list with
+        n elements, where n = board_size
+        """
+
+        board = []
+        for i in range(self.size):
+            board.append([])
+            for j in range(self.size):
+                board[i].append(' ')
+        return board
+
+    def place_move(self, i, j, player):
+        """Handles player moves by
+        updating the current board
+        """
+        try:
+            if isinstance(self.board[i][j], Player):
+                # There is a player instance here already
+                return 'Invalid move, try again!'
+
+            self.board[i][j] = player
+            self.move_count += 1 # helps to check for draw
+            won = self.check_win(i, j, player)
+
+            if isinstance(won, Player):
+                # check_win returns a Player object if someone won
+                self.game_over = True
+                print(self) # 1 print call at the end to display the final board
+                return f'THE WINNER IS {str(player)}   !!!\n\n'
+            if won == 'draw':
+                # If it's a draw it returns 'draw'
+                self.game_over = True
+                print(self)
+                return "It's a draw!"
+
+            # If all went well, we return
+            # the board with modified state so we can print it
+            return self
+        except IndexError:
+            # Move out of bounds
+            return 'Invalid move, try again!'
+
+    def check_win(self, last_move_x, last_move_y, player):
+        """Called after every move to check if someone
+        has won or if the game is a draw. It only checks in
+        rows and cols where the last move was placed.
+        Returns:
+        - player if player won
+        - 'draw' if it's a draw
+        - null otherwise
+        """
+
+        # check rows
+        for i in range(self.size):
+            if self.board[last_move_x][i] != player:
+                break
+            if i == self.size - 1:
+                # Player won
+                return player
+
+        # check col
+        for i in range(self.size):
+            if self.board[i][last_move_y] != player:
+                break
+            if i == self.size - 1:
+                # Player won
+                return player
+
+        # check diagonal
+        # only if last move was on a diagonal
+        if last_move_x == last_move_y:
+            for i in range(self.size):
+                if self.board[i][i] != player:
+                    break
+                if i == self.size - 1:
+                    # Player won
+                    return player
+
+        # check anti diagonal
+        # only if last move was on an anti-diagonal
+        if last_move_x + last_move_y == self.size - 1:
+            for i in range(self.size):
+                if self.board[i][(self.size-1) - i] != player:
+                    break
+                if i == self.size - 1:
+                    # Player won
+                    return player
+
+        # check draw
+        if self.move_count == pow(self.size, 2):
+            # If move count = number of squares on the board,
+            # then its a draw
+            return 'draw'
+
+    def repr_as_simple_string(self):
+        """Old, simpler method for __repr__, before
+        I came up with the nicer one I use now
+        """
         repr_str = '\n'
         for row in self.board:
             if self.size % 3 == 0:
@@ -21,73 +165,10 @@ class Board():
         repr_str += '\n'
         return repr_str
 
-    def generate_board(self):
-
-        board = []
-        
-        for i in range(self.size):
-            board.append([])
-            for j in range(self.size):
-                board[i].append('_')
-        return board
-
-    def place_move(self, i, j, player):
-        try:
-            if isinstance(self.board[i][j], Player):
-                return 'invalid move'
-            self.board[i][j] = player
-            self.move_count += 1
-            
-            won = self.check_win(i, j, player)
-            if isinstance(won, Player):
-                self.game_over = True
-                print(self)
-                return f'{str(player)} won!'
-            if won == 'draw':
-                self.game_over = True
-                print(self)
-                return "It's a draw!"
-            return self
-        except IndexError:
-            return 'invalid move'
-
-    def check_win(self, last_move_x, last_move_y, player):
-
-        # check rows
-        for i in range(self.size):
-            if self.board[last_move_x][i] != player:
-                break
-            if i == self.size - 1:
-                return player
-
-        # check col
-        for i in range(self.size):
-            if self.board[i][last_move_y] != player:
-                break
-            if i == self.size - 1:
-                return player
-
-        # check diagonal
-        if last_move_x == last_move_y:
-            for i in range(self.size):
-                if self.board[i][i] != player:
-                    break
-                if i == self.size - 1:
-                    return player
-
-        # check anti diagonal
-        if last_move_x + last_move_y == self.size - 1:
-            for i in range(self.size):
-                if self.board[i][(self.size-1) - i] != player:
-                    break
-                if i == self.size - 1:
-                    return player
-
-        # check draw
-        if self.move_count == pow(self.size, 2):
-            return 'draw'
-
 class Player():
+    """Simple player class that has a given character (eg. X).
+    It returns that character if we call print() on an instance.
+    """
     def __init__(self, character):
         self.character = character
 
@@ -95,8 +176,5 @@ class Player():
         return self.character
 
     def __str__(self):
-        return self.character
-
-    def to_str(self):
         return self.character
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes import Board, Player
 
 def main():
+    """Main function to run the game."""
 
     board_size = select_board_size()
     board = Board(board_size)
@@ -11,41 +12,55 @@ def main():
     player_b = Player(player_characters[1])
 
     previous_player = player_b
+    # Just so that the switch below works, and
+    # we actually start with player_a
 
     while not board.game_over:
         next_player = player_a if previous_player == player_b else player_b
         round_played = ''
 
-        while round_played in ('', 'invalid move'):
+        while not isinstance(round_played, Board) and not board.game_over:
+            # If move is invalid, the same player goes again
             round_played = play_round(next_player, board)
             print(round_played)
-            
+
         previous_player = next_player
 
 
 def play_round(player, board):
+    """Handles one round for a player.
+    Player needs to input their row and column
+    to place a move.
+    """
 
-    print(f'\nPlayer {player} turn.\n')
+    print(f'\nTurn for {player}\n')
 
     move = []
 
     for i in range(2):
-        move_i = ''
+        # row & col
+        move_i = '' # move input for row or col
         row_or_col = 'row' if i == 0 else 'column'
         while move_i not in [str(x) for x in range(board.size)]:
+            # List of possible numbers to input (eg. ['1', '2', '3'])
             move_i = input(f'Select {row_or_col} (1-{board.size}): ')
             try:
                 move_i = int(move_i) - 1
                 move.append(move_i)
                 break
             except (ValueError, IndexError):
+                # If out of bounds or not a number,
+                # then ask again
                 continue
 
     return board.place_move(move[0], move[1], player)
 
-def select_board_size(): 
+def select_board_size():
+    """Handles board size selection
+    by asking for input."""
     board_size = 0
-    while board_size not in range(2,21):
+    while board_size not in range(2, 21):
+        # Max size is 20 because that still confortably fits on screen.
         try:
             board_size = int(input('Select a board size between 2 and 20: '))
         except ValueError:
@@ -53,23 +68,33 @@ def select_board_size():
     return board_size
 
 def select_characters():
+    """Handles character selection.
+    If a player selects a character,
+    that character is no longer selectable."""
 
     possible_characters = [
-        'X', 'O', '♥️', '✔️', '❌', '💔', '🅾️', '👻', '🎃', '🔥'
+        'X', 'O', '♥️', '✔️', '🅾️'
     ]
 
     player_characters = []
 
     for i in range(1, 3):
+        # For both players (1 & 2)
+        
+        # We need to initialise these locally, because as one player selects,
+        # The number of possible characters change
         select_string = '\n'.join([f'{str(x + 1)}: {possible_characters[x]}'  for x in range(len(possible_characters))])
         error_msg = f'You must select a number between 1-{len(possible_characters)}: '
         player_select = ''
 
         while player_select not in [str(x) for x in range(1, len(possible_characters) + 1)]:
+            # Player select needs to be a number in the range of 
+            # how many characters there are (starting from 1)
             print(select_string)
             player_select = input(f'Player {i} - select your character (type from the numbers above): ')
             try:
-                player_select = int(player_select) - 1
+                player_select = int(player_select) - 1 # because lists are 0-indexed
+                # we need to remove the selected char so the next player can't select it again
                 player_character = possible_characters.pop(player_select)
                 player_characters.append(player_character)
                 print(f'Player {i} - you will play as {player_character}\n')
@@ -80,7 +105,6 @@ def select_characters():
 
     return player_characters
 
-    
 
 if __name__ == "__main__":
     main()
